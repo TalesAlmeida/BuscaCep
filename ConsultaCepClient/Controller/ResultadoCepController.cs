02b385c343962e9c5dc78a4928a1cfab423cc86a
talesalmeida9@gmail.com <talesalmeida9@gmail.com>
2017-12-10 23:24:18 -0200

    Primeiro Commit - Busca Cep Via Web Api

---
+++ ConsultaCepClient/Controller/ResultadoCepController.cs
@@ -0,0 +1,48 @@
+﻿using ConsultaCepClient.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ConsultaCepClient.Controller
+{
+    public class ResultadoCepController
+    {
+        HttpClient client = null;
+        ResultadoCep resultadoCep = null;
+        string viaCep = "https://viacep.com.br/ws/";//Api Via Cep
+
+
+        //METODO CONSULTA CEP
+        public ResultadoCep ConsultaCep(string cep)
+        {
+            try
+            {
+                using (client = new HttpClient())
+                {
+                    HttpResponseMessage resposta = client.GetAsync(viaCep+cep+"/json/").Result;
+
+                    if (resposta.IsSuccessStatusCode)
+                    {
+                        resultadoCep = new ResultadoCep();
+                        resultadoCep =  resposta.Content.ReadAsAsync<ResultadoCep>().Result;
+                    }
+                    else
+                    {
+                        resultadoCep = null;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return resultadoCep;
+        }
+
+    }
+}
---
+++ ConsultaCepClient/View/FormPrincipal.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using ConsultaCepClient.Controller;
+using ConsultaCepClient.Model;
+
+namespace ConsultaCepClient
+{
+    public partial class FromPrincipal : Form
+    {
+        public FromPrincipal()
+        {
+            InitializeComponent();
+        }
+        ResultadoCepController consultaCepController = new ResultadoCepController();
+
+        //BOTÃO CONSULTAR CEP
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var resultadoCep = new ResultadoCep();
+                resultadoCep = consultaCepController.ConsultaCep(maskCep.Text);
+
+                if (resultadoCep.Cep != null)
+                {
+                    MessageBox.Show("Resultado:\n" + resultadoCep);
+                }
+                else
+                {
+                    MessageBox.Show("Cep não encontrado !");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro:\n" + ex.Message);
+
+            }
+
+        }
+    }
+}
